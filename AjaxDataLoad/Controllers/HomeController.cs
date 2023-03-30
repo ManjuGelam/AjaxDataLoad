014294c915ee0d3fade4ed780d4c6c0e185ab6c8
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 17:35:52 +0530

    Add project files.

---
+++ AjaxDataLoad/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxDataLoad.Models;
+
+
+namespace AjaxDataLoad.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+              
+
+                List<Product> ProductList = db.Products.ToList<Product>();
+                return Json(new { data = ProductList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
